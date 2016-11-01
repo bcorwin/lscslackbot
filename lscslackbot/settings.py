eff8f01cc05616d3d69a669ac8f6cd4a4507d4d6
Ben (home) <bscorwin@gmail.com>
2016-11-01 01:01:39 -0500

    Debug on

--- lscslackbot/settings.py
+++ lscslackbot/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-TEMPLATE_DEBUG = False
+DEBUG = True
+TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
