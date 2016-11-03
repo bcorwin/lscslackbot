62b03f44b541bd00fbf4ac0f6e1e4dd8efe51c34
Ben (home) <bscorwin@gmail.com>
2016-11-03 16:02:50 -0500

    Fixed database def

--- lscslackbot/settings.py
+++ lscslackbot/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
+import os, sys
+sys.path.append("C:\Anaconda\Lib\site-packages")
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -73,12 +75,8 @@ WSGI_APPLICATION = 'lscslackbot.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
